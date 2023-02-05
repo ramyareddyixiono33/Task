2c75ddfcdd9a602a16df0cb91dac07ae1061bfa5
ramyareddyixiono33 <ramyaixiono@gmail.com>
2023-02-05 15:40:21 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Navbar from './component/Navbar'; 
+import Nav2 from'./component/Page1';
+import Nav3 from'./component/Page2';
+import Nav4 from'./component/Page3';
+import Completed from './component/Page4';
+
+export default function App() {
+  return (
+   <>
+  <Nav2/>
+  <Nav3/>
+  <Nav4/>
+  <Navbar/>
+  <Completed/>
+   </>
+         )
+}
